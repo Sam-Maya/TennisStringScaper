2a2aea8447a148d828318ede179bf65334a10ad6
Sam Maya <samuelmaya112@gmail.com>
2022-10-12 13:05:23 -0400

    added tennisString class

---
+++ classses.js
@@ -0,0 +1,9 @@
+class TennisString{
+  constructor(name, material, stiffness, tensionLoss, spinPotential){
+    this.name = name
+    this.material = material
+    this.stiffness = stiffness
+    this.tensionLoss = tensionLoss
+    this.spinPotential = spinPotential
+  }
+}
